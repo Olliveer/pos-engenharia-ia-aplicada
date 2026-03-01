5ad50b54b7670d82e0a81724398dcca16fb98fb9
Olliveer <j.oliveira90@live.com>
2026-02-28 22:08:40 -0300

    MÓDULO 03 - COMO FUNCIONAM SISTEMAS DE RECOMENDAÇÃO PT05

--- fundamentos-de-IA-e-LLMs-para-programadores/module-03/exemplo-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
+++ fundamentos-de-IA-e-LLMs-para-programadores/module-03/exemplo-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
@@ -3,6 +3,7 @@ import { workerEvents } from "../events/constants.js";
 
 console.log("Model training worker initialized");
 let _globalCtx = {};
+let _model = {};
 
 const WEIGHTS = {
   category: 0.4,
@@ -109,21 +110,25 @@ function encodeUser(user, ctx) {
 function createTrainingData(ctx) {
   const inputs = [];
   const labels = [];
-  ctx.users.forEach((user) => {
-    const userVector = encodeUser(user, ctx).dataSync();
-    ctx.products.forEach((product) => {
-      const productVector = encodeProduct(product, ctx).dataSync();
+  ctx.users
+    .filter((u) => u.purchases.length)
+    .forEach((user) => {
+      const userVector = encodeUser(user, ctx).dataSync();
+      ctx.products.forEach((product) => {
+        const productVector = encodeProduct(product, ctx).dataSync();
 
-      const label = user.purchases.some((p) => p.name === product.name) ? 1 : 0;
+        const label = user.purchases.some((p) => p.name === product.name)
+          ? 1
+          : 0;
 
-      // Aqui você pode criar um dataset de treinamento usando userVector, productVector e label
-      // Por exemplo, você pode concatenar userVector e productVector para criar uma entrada combinada
-      // E usar label como a saída esperada para treinar um modelo de classificação binária
-      // Exemplo de como criar uma entrada combinada:
-      inputs.push([...userVector, ...productVector]);
-      labels.push(label);
+        // Aqui você pode criar um dataset de treinamento usando userVector, productVector e label
+        // Por exemplo, você pode concatenar userVector e productVector para criar uma entrada combinada
+        // E usar label como a saída esperada para treinar um modelo de classificação binária
+        // Exemplo de como criar uma entrada combinada:
+        inputs.push([...userVector, ...productVector]);
+        labels.push(label);
+      });
     });
-  });
 
   return {
     xs: tf.tensor2d(inputs),
@@ -131,6 +136,76 @@ function createTrainingData(ctx) {
     // A dimensão de entrada para o modelo seria a soma das dimensões do vetor do usuário e do vetor do produto
     inputDimention: ctx.dimentions * 2,
   };
+}
+
+async function configureNeuralNetAndTrain(trainData) {
+  const model = tf.sequential();
+  model.add(
+    tf.layers.dense({
+      // A dimensão de entrada é a soma das dimensões do vetor do usuário e do vetor do produto
+      inputShape: [trainData.inputDimention],
+      // A dimensão de saída é 1, pois estamos fazendo uma classificação binária (compra ou não compra)
+      units: 128,
+      // Usamos ReLU como função de ativação para introduzir não linearidade
+      // ReLU é uma escolha comum para camadas ocultas em redes neurais,
+      // pois ajuda a evitar o problema de gradientes desaparecendo
+      // e permite que o modelo aprenda representações mais complexas dos dados.
+      activation: "relu",
+    }),
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 64,
+      activation: "relu",
+    }),
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 32,
+      activation: "relu",
+    }),
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 1,
+      // Usamos sigmoid para a camada de saída em classificação binária
+      // A função sigmoid mapeia a saída para um valor entre 0 e 1,
+      // que pode ser interpretado como a probabilidade de compra.
+      activation: "sigmoid",
+    }),
+  );
+
+  model.compile({
+    optimizer: tf.train.adam(0.01),
+    // Usamos binaryCrossentropy como função de perda para classificação binária
+    // A função de perda binaryCrossentropy mede a diferença entre as probabilidades previstas e os rótulos reais,
+    // e é adequada para problemas de classificação binária.
+    loss: "binaryCrossentropy",
+    metrics: ["accuracy"],
+  });
+
+  await model.fit(trainData.xs, trainData.ys, {
+    // O número de epochs é o número de vezes que o modelo verá todo o conjunto de treinamento durante o processo de aprendizado.
+    epochs: 10,
+    // O batchSize é o número de amostras que serão propagadas através da rede antes de atualizar os pesos do modelo.
+    batchSize: 32,
+    shuffle: true,
+    callbacks: {
+      onEpochEnd: (epoch, logs) => {
+        postMessage({
+          type: workerEvents.trainingLog,
+          epoch: epoch,
+          loss: logs.loss,
+          accuracy: logs.acc,
+        });
+      },
+    },
+  });
+
+  return model;
 }
 
 async function trainModel({ users }) {
@@ -157,23 +232,13 @@ async function trainModel({ users }) {
   _globalCtx = context;
 
   const trainingData = createTrainingData(context);
-
-  debugger;
+  _model = await configureNeuralNetAndTrain(trainingData);
 
   postMessage({
-    type: workerEvents.trainingLog,
-    epoch: 1,
-    loss: 1,
-    accuracy: 1,
+    type: workerEvents.progressUpdate,
+    progress: { progress: 100 },
   });
-
-  setTimeout(() => {
-    postMessage({
-      type: workerEvents.progressUpdate,
-      progress: { progress: 100 },
-    });
-    postMessage({ type: workerEvents.trainingComplete });
-  }, 1000);
+  postMessage({ type: workerEvents.trainingComplete });
 }
 function recommend(user, ctx) {
   console.log("will recommend for user:", user);
